3569e7ecacd57174673eca69ba8d5e53f5883c95
Duronne <duronne.tks@gmail.com>
2023-10-17 21:31:33 -0300

    feat: add prisma repository

---
+++ src/repositories/prisma/prisma-pets-adoption-requirements-repository.ts
@@ -0,0 +1,19 @@
+import { AdoptionRequirements, Prisma } from '@prisma/client'
+import { PetsAdoptionRequirementsRepository } from '../pets-adoption-requirements-repository'
+import { prisma } from '@/lib/prisma'
+
+export class InMemoryPetsAdoptionRequerimentsRepository
+  implements PetsAdoptionRequirementsRepository
+{
+  public items: AdoptionRequirements[] = []
+
+  async create(
+    data: Prisma.AdoptionRequirementsUncheckedCreateInput,
+  ): Promise<AdoptionRequirements> {
+    const adoptionRequirements = await prisma.adoptionRequirements.create({
+      data,
+    })
+
+    return adoptionRequirements
+  }
+}
