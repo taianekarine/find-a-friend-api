3957957528fada40f29fbd6bce0290da7fff328f
Duronne <duronne.tks@gmail.com>
2023-10-18 02:25:01 -0300

    feat: add test to authenticate

--- src/http/controllers/orgs/authenticate.spec.ts
+++ src/http/controllers/orgs/authenticate.spec.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('Register (e2e)', () => {
+describe('Authenticate (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,8 +11,8 @@ describe('Register (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to register', async () => {
-    const response = await request(app.server).post('/orgs').send({
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/orgs').send({
       name: 'Org Test',
       email: 'orgtest@example.com',
       password: '123456',
@@ -21,6 +21,14 @@ describe('Register (e2e)', () => {
       whatsappNumber: '(99) 99999-9999',
     })
 
-    expect(response.statusCode).toEqual(201)
+    const response = await request(app.server).post('/sessions').send({
+      email: 'orgtest@example.com',
+      password: '123456',
+    })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
   })
 })
